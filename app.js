dd68e3023af46f59848d897839d17c95e82b5ac8
Brandon <estanob@gmail.com>
2021-02-23 23:09:28 -0800

    Change listen

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get("/", (req, res) => {
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-const port = process.env.PORT || 5000;
+const port = (process.env.PORT || 5000);
 
 app.listen(port, () => {console.log(`Listening on ${port}`)});
